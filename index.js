bcbbe0e07db0974a77b8c5107ac60dbe6aec81c7
AbyanAhmad <abyanhmd2@gmail.com>
2021-06-04 09:17:08 +0700

    add history

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mysql = require("mysql");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const app = express();
 
 app.use(express.json());
@@ -12,10 +14,10 @@ app.get("/", async (req, res) => {
     res.json({status:"Ready to roll!"});
 });
 
-app.get("/:person", async (req, res) => {
-    const query = "SELECT * FROM person WHERE name = ?";
+app.get("/:user", async (req, res) => {
+    const query = "SELECT * FROM users WHERE name = ?";
     console.log("")
-    pool.query(query, [ req.params.person ], (error, result) => {
+    pool.query(query, [ req.params.user ], (error, result) => {
         if (!result[0]) {
             res.json({status:"Not Found!"});
         } else {
@@ -31,14 +33,19 @@ const pool = mysql.createPool({
     socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
 }); 
 
-app.post("/", async (req, res) => {
+app.post("/register", async (req, res) => {
+    const password = req.body.password;
+    const email = req.body.email;
+    const name = req.body.name;
+    const encryptedPassword = await bcrypt.hash(password, saltRounds)
+
     const data = {
-        name: req.body.name,
-        age: req.body.age,
-        gender: req.body.gender
+        "name": name,
+        "email": email,
+        "password": encryptedPassword,
     }
-    const query = "INSERT INTO person VALUES (?, ?, ?)";
-    pool.query(query, Object.values(data), (error) => {
+    const query = "INSERT INTO users SET ?";
+    pool.query(query, data, (error) => {
         if(error) {
             res.json({status: "failure", reason: error.code});
         } else {
@@ -46,3 +53,32 @@ app.post("/", async (req, res) => {
         }
     });
 });
+
+app.post("/history", async (req, res) => {
+    const data = {
+        "id": req.body.id,
+        "symptoms": req.body.symptoms,
+        "illness": req.body.illness,
+        "accuracy": req.body.accuracy
+    }
+    const query = "INSERT INTO history SET ?";
+    pool.query(query, data, (error) => {
+        if(error) {
+            res.json({status: "failure", reason: error.code});
+        } else {
+            res.json ({status: "success", data: data});
+        }
+    });
+});
+
+app.get("/history/:uid", async (req, res) => {
+    const query = "SELECT TOP(10) * FROM history WHERE id=?  ORDER BY reg_date DESC";
+    pool.query(query, [ req.params.uid ] , (error, result) => {
+        if (!result[0]) {
+            res.json({status:"Not Found!"});
+        } else {
+            res.json(result[0]);
+        }
+    });
+});
+
